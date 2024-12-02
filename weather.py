ed0784aac632734ebcf6cc695f7c6a55f0d07d56
sital-tharu <sitatharu769@gmail.com>
2024-12-02 20:28:10 +0530

    to  see the information

--- weather.py
+++ weather.py
@@ -4,4 +4,5 @@ city_name=input('Enter a city Name:')
 api_key='2c0c2746adaec43b0a00589692820720'
 
 api_url=f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'
-
+get_server_information=requests.get(api_url)
+print(get_server_information.json())
