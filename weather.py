bb5d90b643cb0b15c437a34728bfe136e9a40c0b
sital-tharu <sitatharu769@gmail.com>
2024-12-02 19:03:31 +0530

    add url of api

--- weather.py
+++ weather.py
@@ -1,3 +1,7 @@
 import json
 import requests
 city_name=input('Enter a city Name:')
+api_key='2c0c2746adaec43b0a00589692820720'
+
+api_url=f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+
